8c52d14069ea5e056e070fd3c69d1fef6c9571ec
isaichenko <isaichenko.e@gmail.com>
2022-06-11 16:31:37 +0300

    first commit

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,36 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator" do 
+      argument :name, String, required: true
+    end
+    
+    def test_field(name:)
+      Rails.logger.info context[:time]
+      "Hello #{name}!"
+    end
+
+    field :author, Types::AuthorType, null: true,
+      description: "Returns one Author instance" do 
+      argument :id, ID, required: true
+    end
+
+    def author(id:)
+      Author.find(id)
+    end
+
+    field :authors, [Types::AuthorType], null: false
+
+    def authors
+      Author.all
+    end
+  end
+end
